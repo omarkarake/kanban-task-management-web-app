4282c699bf2a4e68bdbbbd59a186df887618579d
omar karake <omar.karake@amalitechtraining.org>
2024-09-17 13:05:30 +0200

    working on the mobile sidebar

--- src/app/layout/navigation-phone/navigation-phone.component.ts
+++ src/app/layout/navigation-phone/navigation-phone.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navigation-phone',
   templateUrl: './navigation-phone.component.html',
-  styleUrl: './navigation-phone.component.css'
+  styleUrl: './navigation-phone.component.css',
 })
 export class NavigationPhoneComponent {
+  items = ['Platform Launch', 'Other Item', 'Roadmap', '+ Create New Board'];
+  selectedItemIndex: number = 0;
+  isDarkMode: Observable<boolean> = this.themeService.isDarkMode$;
+  dropDownOpen: boolean = false;
+  constructor(private themeService: ThemeService) {}
 
+  selectItem(index: number): void {
+    this.selectedItemIndex = index;
+    this.dropDownOpen = false;
+  }
+  toggleDarkMode(): void {
+    this.themeService.toggleTheme();
+  }
+  toggleDropDown(): void {
+    this.dropDownOpen = !this.dropDownOpen;
+  }
 }
