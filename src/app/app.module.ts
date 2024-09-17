31f6cb1ddee30a70276b82f5b9fc0e64804d1ce3
omar karake <omar.karake@amalitechtraining.org>
2024-09-17 10:12:42 +0200

    working on the input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ButtonlComponent } from './components/buttons/buttonl/buttonl.component
 import { ButtonseComponent } from './components/buttons/buttonse/buttonse.component';
 import { ButtonsdeComponent } from './components/buttons/buttonsde/buttonsde.component';
 import { SubtaskCheckboxComponent } from './components/subtask-checkbox/subtask-checkbox.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SubtaskCheckboxComponent } from './components/subtask-checkbox/subtask-
     ButtonlComponent,
     ButtonseComponent,
     ButtonsdeComponent,
-    SubtaskCheckboxComponent
+    SubtaskCheckboxComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+})
+export class InputComponent {
+  @Input() label: string = 'Text Field (Idle)';
+  @Input() placeholder: string = 'Placeholder';
+  @Input() type: string = 'text';
+  @Input() value: string = '';
+  @Input() error: boolean = false;
+}
