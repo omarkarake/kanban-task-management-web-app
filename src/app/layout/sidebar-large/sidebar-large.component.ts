47d79f1ec9fbdd96334ac0e73427a6b2ad21d368
omar karake <omar.karake@amalitechtraining.org>
2024-09-17 12:40:33 +0200

    workin on the sidebar for large screen

--- src/app/layout/sidebar-large/sidebar-large.component.ts
+++ src/app/layout/sidebar-large/sidebar-large.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar-large',
@@ -8,8 +10,13 @@ import { Component } from '@angular/core';
 export class SidebarLargeComponent {
   items = ['Platform Launch', 'Other Item', 'Roadmap', '+ Create New Board'];
   selectedItemIndex: number = 0;
+  isDarkMode: Observable<boolean> = this.themeService.isDarkMode$;
+  constructor(private themeService: ThemeService) {}
 
   selectItem(index: number): void {
     this.selectedItemIndex = index;
+  }
+  toggleDarkMode(): void {
+    this.themeService.toggleTheme();
   }
 }
