2ea39d88f4e901d5634b2000ccc46c3c8aaf01b3
omar karake <omar.karake@amalitechtraining.org>
2024-09-19 20:57:23 +0200

    feat(column-component): add logic to render subtasks and completed subtask count
    - Moved subtask filtering logic from the template to the TypeScript file to avoid Angular parser errors.
    - Created getCompletedSubtasksCount() to calculate and return the number of completed subtasks per task.
    - Created getTotalSubtasksCount() to return the total number of subtasks for a task.
    - Updated the template to use the new methods for rendering the subtask count and completed subtasks.
    - Ensured that subtasks are handled dynamically and displayed correctly for each task in the column component.
    - Improved performance and code clarity by removing complex template expressions and moving logic to the component class.

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from '../../services/theme/modal/modal.service';
+import { Subtask, Task } from '../../models/boards.modal';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
   styleUrl: './column.component.css',
 })
-export class ColumnComponent {
+export class ColumnComponent implements OnInit {
   // title can be: 'todo', 'doing', 'done'
-  @Input() title: string = 'done';
-  @Input() tasks: string[] = ['Task 1', 'Task 2'];
+  @Input() title: string = 'todo'; // Title of the column
+  @Input() tasks: Task[] = []; // Tasks associated with the column
+  subtasks: Subtask[] = []; // Subtasks associated with the task
   constructor(private modalService: ModalService) {}
+
+  ngOnInit(): void {
+    this.tasks.forEach((task) => {
+      this.subtasks.push(...task.subtasks);
+    });
+
+    console.log("subtasks are: ", this.subtasks);
+  }
   openModal() {
     this.modalService.openModal('view-task');
+  }
+
+  getCompletedSubtasksCount(task: Task): number {
+    return task.subtasks ? task.subtasks.filter(subtask => subtask.isCompleted).length : 0;
+  }
+
+  getTotalSubtasksCount(task: Task): number {
+    return task.subtasks ? task.subtasks.length : 0;
   }
 }
