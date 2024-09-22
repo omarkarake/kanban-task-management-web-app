996467054a9a675dc365ee9b3db81d9ac9814f03
omar karake <omar.karake@amalitechtraining.org>
2024-09-22 19:06:01 +0200

    test: testing button seconddary

--- src/app/components/buttons/buttonse/buttonse.component.spec.ts
+++ src/app/components/buttons/buttonse/buttonse.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonseComponent } from './buttonse.component';
 
 describe('ButtonseComponent', () => {
@@ -8,16 +7,39 @@ describe('ButtonseComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ButtonseComponent]
-    })
-    .compileComponents();
+      declarations: [ ButtonseComponent ],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ButtonseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display default text "ButtonS"', () => {
+    expect(component.text).toBe('ButtonS');
+  });
+
+  it('should accept custom text as input', () => {
+    component.text = 'Submit';
+    fixture.detectChanges();
+    expect(component.text).toBe('Submit');
+  });
+
+  it('should apply additional classes passed as input', () => {
+    component.additionalClasses = 'btn-danger';
+    fixture.detectChanges();
+    expect(component.additionalClasses).toBe('btn-danger');
+  });
+
+  it('should accept custom type as input', () => {
+    component.type = 'submit';
+    fixture.detectChanges();
+    expect(component.type).toBe('submit');
   });
 });
