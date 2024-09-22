50a32eaf000ad6f22905a8fd932981fc7f9e6348
omar karake <omar.karake@amalitechtraining.org>
2024-09-22 20:12:33 +0200

    test(largenav-service): add unit tests for LargenavService using Jest
    - Added tests to verify the initial value of the header and toggleIconLogo BehaviorSubjects.
    - Tested the ability to update the header and toggleIconLogo values using .next().
    - Ensured that the service correctly emits updated values through observables.

--- src/app/services/navigation/largenav.service.spec.ts
+++ src/app/services/navigation/largenav.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LargenavService } from './largenav.service';
 
 describe('LargenavService', () => {
   let service: LargenavService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LargenavService],
+    });
     service = TestBed.inject(LargenavService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should initialize header with "Platform Launch"', () => {
+    service.header$.subscribe((value) => {
+      expect(value).toBe('Platform Launch');
+    });
+  });
+
+  it('should allow updating the header value', () => {
+    service.header.next('New Header');
+    service.header$.subscribe((value) => {
+      expect(value).toBe('New Header');
+    });
+  });
+
+  it('should initialize toggleIconLogo with false', () => {
+    service.togoLogo$.subscribe((value) => {
+      expect(value).toBe(false);
+    });
+  });
+
+  it('should allow updating the toggleIconLogo value', () => {
+    service.toggleIconLogo.next(true);
+    service.togoLogo$.subscribe((value) => {
+      expect(value).toBe(true);
+    });
   });
 });
