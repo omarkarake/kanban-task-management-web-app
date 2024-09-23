faa9a4c11e5db373bb2fa1cfc604325b5564a3ba
omar karake <omar.karake@amalitechtraining.org>
2024-09-23 18:20:19 +0200

    feat: Implement drag-and-drop with move icon visibility during drag
    - Added Angular CDK drag-and-drop functionality to the ColumnComponent for task reordering and task transfer between columns.
    - Introduced logic to show a move  icon only while dragging tasks.
    - Updated the component to track the dragging state using isDragging flag.
    - Integrated theme support for the move icon to switch between dark and light mode icons, using the isDarkMode$ observable from ThemeService.
    - Added (cdkDragStarted) and (cdkDragEnded) event handlers to manage the display of the move icon during the drag process.
    - The move icon is displayed based on the current theme, switching between 'icon-move.svg' for light mode and 'icon-move-dark.svg' for dark mode.
    - The task order is updated immutably when tasks are reordered within the same column or moved to a different column.
    - Dispatched updateTaskStatus action to update the task's status when moved between columns.

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -2,9 +2,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from '../../services/theme/modal/modal.service';
 import { Subtask, Task } from '../../models/boards.modal';
 import { AppComponent } from '../../app.component';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Store } from '@ngrx/store';
 import { updateTaskStatus } from '../../store/actions/boards.actions';
+import { Observable } from 'rxjs';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-column',
@@ -14,15 +20,20 @@ import { updateTaskStatus } from '../../store/actions/boards.actions';
 export class ColumnComponent implements OnInit {
   @Input() title: string = 'todo'; // Title of the column
   @Input() tasks: Task[] = []; // Tasks associated with the column
+  @Input() isDarkTheme: boolean = false; // Input to determine if the theme is dark
   subtasks: Subtask[] = [];
+  isDragging: boolean = false; // Flag to track dragging state
+  isDarkMode$!: Observable<boolean>;
 
   constructor(
     private modalService: ModalService,
     private parentComponent: AppComponent,
+    private themeService: ThemeService,
     private store: Store
   ) {}
 
   ngOnInit(): void {
+    this.isDarkMode$ = this.themeService.isDarkMode$;
     this.tasks.forEach((task) => {
       this.subtasks.push(...task.subtasks);
     });
@@ -34,7 +45,7 @@ export class ColumnComponent implements OnInit {
       // If dropped in the same container, reorder the array immutably
       const updatedTasks = [...event.container.data];
       moveItemInArray(updatedTasks, event.previousIndex, event.currentIndex);
-      
+
       // Assign the updated tasks array immutably
       this.tasks = updatedTasks;
     } else {
@@ -42,22 +53,31 @@ export class ColumnComponent implements OnInit {
       const prevTasks = [...event.previousContainer.data];
       const currentTasks = [...event.container.data];
       const task = prevTasks[event.previousIndex];
-  
-      transferArrayItem(prevTasks, currentTasks, event.previousIndex, event.currentIndex);
-      
+
+      transferArrayItem(
+        prevTasks,
+        currentTasks,
+        event.previousIndex,
+        event.currentIndex
+      );
+
       // Assign the updated tasks arrays immutably
       this.tasks = currentTasks;
-  
+
       // Dispatch action to update the task's status (column name) immutably
-      this.store.dispatch(updateTaskStatus({
-        taskId: task.title, // or another unique ID for the task
-        newStatus: this.title // Set the new column's title as the new status
-      }));
+      this.store.dispatch(
+        updateTaskStatus({
+          taskId: task.title, // or another unique ID for the task
+          newStatus: this.title, // Set the new column's title as the new status
+        })
+      );
     }
   }
 
   getCompletedSubtasksCount(task: Task): number {
-    return task.subtasks ? task.subtasks.filter(subtask => subtask.isCompleted).length : 0;
+    return task.subtasks
+      ? task.subtasks.filter((subtask) => subtask.isCompleted).length
+      : 0;
   }
 
   getTotalSubtasksCount(task: Task): number {
@@ -66,5 +86,15 @@ export class ColumnComponent implements OnInit {
 
   openTaskModal(task: Task) {
     this.parentComponent.openTaskModal(task);
+  }
+
+  // Called when dragging starts
+  onDragStart(): void {
+    this.isDragging = true;
+  }
+
+  // Called when dragging ends
+  onDragEnd(): void {
+    this.isDragging = false;
   }
 }
